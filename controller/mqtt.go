0a248a7db29e04d2e03283e31b3bbdee8e8eee18
ekharisma <ekharisma@outlook.com>
2021-12-10 20:08:09 +0700

    protobuf package

--- controller/mqtt.go
+++ controller/mqtt.go
@@ -1,8 +1,7 @@
-package main
+package controller
 
 import (
 	"fmt"
-	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
@@ -10,41 +9,29 @@ import (
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
 }
+
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
 	fmt.Println("Connected")
 }
-var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-	fmt.Printf("Connect lost: %v", err)
+
+func subscribe(client mqtt.Client) {
+	topic := "iot/thermostat"
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s", topic)
 }
 
-func main() {
+func Init() {
 	var broker = "broker.hivemq.com"
 	var port = 1883
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
-	opts.SetDefaultPublishHandler(messagePubHandler)
 	opts.OnConnect = connectHandler
-	opts.OnConnectionLost = connectLostHandler
+	opts.SetDefaultPublishHandler(messagePubHandler)
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
-	sub(client)
-	publish(client)
+	subscribe(client)
 	client.Disconnect(250)
-}
-func publish(client mqtt.Client) {
-	num := 10
-	for i := 0; i < num; i++ {
-		text := fmt.Sprintf("Message %d", i)
-		token := client.Publish("topic/test", 0, false, text)
-		token.Wait()
-		time.Sleep(time.Second)
-	}
-}
-func sub(client mqtt.Client) {
-	topic := "iot/thermostat"
-	token := client.Subscribe(topic, 1, nil)
-	token.Wait()
-	fmt.Printf("Subscribed to topic: %s\n", topic)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module ekharisma/challenge
 
 go 1.13
 
-require github.com/eclipse/paho.mqtt.golang v1.3.5 // indirect
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.5
+	github.com/golang/protobuf v1.5.2 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
